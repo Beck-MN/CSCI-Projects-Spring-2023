b91efa5c134a7765f511d56f6e34c09944024bc4
Beck-MN <Beck.christensen@gmail.com>
2023-04-30 23:23:39 -0500

    added comments

--- CSCI 301/map.cpp
+++ CSCI 301/map.cpp
@@ -8,14 +8,18 @@ using namespace std;
 
 int main { 
 
+//creat a map of strings to integers
 map<string, int> map;
 
+//Insert some values into the map
 map["one"] = 1;
 map["two"] = 2;
 map["three"] = 3;
 
+//get an iterator pointing to the first element in the map
 map<string, int>::interator it = map.begin();
 
+//iterate through map and print values
 while (it != map.end()) {
 
 	cout << "Key: " << it->first << ", Value: " << it->second << endl;
