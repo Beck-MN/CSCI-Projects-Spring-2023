7c2d5cf4cf3e0f6f1d96a3efde61453b1d6c7f52
Beck Christensen <Beck.christensen@gmail.com>
2023-05-07 18:25:41 -0500

    updated calc to loop

--- CSCI 301/Practice/calculator_practice.cpp
+++ CSCI 301/Practice/calculator_practice.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 
-//Beck Christensen, Sun May 7th, practice program
+// Beck Christensen, Sun May 7th, practice program
 
 /*Basic calulator, mainly just testing out vs code on my laptop and trying out a new keyboard.
 Takes in a line of two number seperated by an opperator and spits out the result*/
@@ -37,28 +37,34 @@ int main()
 
     double num, num2;
     char opp = ' ';
+    char start = 'y';
 
-    cout << "Please enter an equation: " << endl;
-    cin >> num >> opp >> num2;
+    while (start = 'y')
+    {
 
-    if (opp == '+')
-    {
-        num = add(num, num2);
-    }
-    else if (opp == '-')
-    {
-        num = sub(num, num2);
-    }
-    else if (opp == '*')
-    {
-        num = mult(num, num2);
-    }
-    else
-    {
-        num = div(num, num2);
-    }
+        cout << "Please enter an equation: " << endl;
+        cin >> num >> opp >> num2;
 
-    cout << "Your number is: " << num << endl;
+        if (opp == '+')
+        {
+            num = add(num, num2);
+        }
+        else if (opp == '-')
+        {
+            num = sub(num, num2);
+        }
+        else if (opp == '*')
+        {
+            num = mult(num, num2);
+        }
+        else
+        {
+            num = div(num, num2);
+        }
 
+        cout << "Your number is: " << num << endl;
+        cout << "Do you want to continue?: ";
+        cin >> start;
+    }
     return 0;
 }
